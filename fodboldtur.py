141d0276fa206e9a1df8e83a5566be15c2c05a0b
wolfyace <56107226+wolfyace@users.noreply.github.com>
2021-10-01 15:18:51 +0200

    så

--- fodboldtur.py
+++ fodboldtur.py
@@ -18,17 +18,17 @@ def menu():
     print("MENU")
     print("1: se listen")
     print("2: redigere beløb")
-    print("3: se hvem der mangler at betale det fulde beløb")
+    print("3: se hvor meget der mangler at blive betalt")
     print("4: Afslut program")
-
+# lige oven over fortæller vi hvad der skal være med i vors menu
     valg = input("Indtast dit valg:")
-    if (valg == '1'):
+    if (valg == '1'): # if statement hvis man vælger 1 i terminalen så skal den printe listen for holdet og menuen igen
         printliste()
         menu()
 
     if (valg == '2'):
-        printliste()
-        Navn = input("hvem vil du ændre beløb på")
+        printliste()# den printe listen så du har et overbilk
+        Navn = input("hvem vil du ændre beløb på") #her bliver der fortalt at den skal sige det som står i input parentesen hvis du vælger 2
         if Navn in fodboldtur:
             beløb = int(input("hvor meget vil du ændre beløbet med?"))
             fodboldtur[Navn] += beløb
@@ -39,8 +39,10 @@ def menu():
             menu()
 
     if (valg == '3'):
-        printliste()
-        
+        sum = 0
+        for key in fodboldtur.keys():
+            sum += fodboldtur[key]
+        print(sum-4500)
 
         menu()
 
